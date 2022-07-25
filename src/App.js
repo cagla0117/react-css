07f94c23ddf586808789d4f55bcc8e31d5cd0b7b
cagla0117 <midiklicagla@gmail.com>
2022-07-25 11:02:51 +0300

    using css in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.module.css';
+import Form from './components/Form/Form';
+import Users from './components/Users/Users';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <h2 className={styles.title}>title</h2>
+      <Users></Users>
+      <Form></Form>
     </div>
   );
 }
